fd748d882eaa180d7069195ba6d98ac20530f190
编程丁真 <1538939880@qq.com>
2025-03-13 20:01:37 +0800

    微信小程序

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,25 +1,31 @@
 package com.sky.config;
 
-
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
 public class RedisConfiguration {
 
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        log.info("开始创建redis模版对象……");
-        RedisTemplate redisTemplate = new RedisTemplate();
-        //设置redis连接工厂对象
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        //设置redis，key的序列化器
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建redis模板类...");
+    RedisTemplate redisTemplate = new RedisTemplate();
+    // 设置Key的序列化器，默认为JdkSerializationRedisSerializer
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+     return redisTemplate;
+}
 }
